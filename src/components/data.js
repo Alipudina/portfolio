4a8d4c765d79044496655d0f336a0cf7edffe1ee
Alipudina <alipudina333@gmail.com>
2019-07-08 14:28:36 +0200

    changing phone number

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,5 @@
 export const contactData = [
   {ident: 'Email', name: 'alipudina55@gmail.com'},
-  {ident: 'Tel', name: '004917634125333'},
+  {ident: 'Tel', name: '004917643813929'},
   {ident: 'Address', name: 'Oliver-lißy-str 40, 22523 Hamburg'}
 ]
